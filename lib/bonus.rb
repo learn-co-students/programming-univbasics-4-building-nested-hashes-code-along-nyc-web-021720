c75043e6a7d3f6f0c888f8dd7fba0722f36cd2a1
jolly-assembler-1925 <jnzhang622@gmail.com>
2020-02-03 02:34:20 +0000

    Done.

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -19,12 +19,11 @@ def bonus
       ]
    }
   }
-bonus[:montague][:hero][:status] = "dead"
-bonus[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
   
-
   
-
+  
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
